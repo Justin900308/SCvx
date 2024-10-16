20d6d8bd54c16174c04c446c03dc37e2dc83a58a
Justin900308 <wow3987@gmail.com>
2024-10-15 21:15:08 -0700

    Add files via upload

--- scvx.m
+++ scvx.m
@@ -28,8 +28,40 @@ end
 
 XX=reshape(X(:,1:end-1),[length((X(:,1)))*length(X(1,:))-4,1]);
 uu=reshape(u,[length((u(:,1)))*length(u(1,:)),1]);
-XX=cat_vec(X,u);
+z=stack_vec(X,u);
 plot(X(1,:),X(2,:),'.')
+hold on
+
+%%
+
+
+cvx_begin
+
+    variable uu(102,1)
+    variable XX(204,1)
+    minimize(norm(uu,2))
+
+    XX(1:4)==[0;0;0;0];
+    uu(1:2)==[0;0];
+    for i=2:51
+        XX((i-1)*4+1:(i-1)*4+4)==Ad*XX((i-1-1)*4+1:(i-1-1)*4+4)+Bd*uu((i-1-1)*2+1:(i-1-1)*2+2);
+        -1<=uu((i-1-1)*2+1)<=1;
+        -1<=uu((i-1-1)*2+1)<=1;
+    end
+
+
+    XX(201:204)==[3.2225;4.5805;0;0];
+
+cvx_end
+
+[X,u]=unstack_vec(XX,uu);
+
+plot(X(1,:),X(2,:),'.')
+
+
+
+
+
 
 
 %% 
@@ -45,8 +77,14 @@ pretty(diff(D,y))
 %% utility funcitons
 
 
-function XX=cat_vec(X,u)
+function XX=stack_vec(X,u)
     XX=reshape(X(:,1:end-1),[length((X(:,1)))*length(X(1,:))-4,1]);
     uu=reshape(u,[length((u(:,1)))*length(u(1,:)),1]);
     XX=[XX;uu];
+end
+
+
+function [X,u]=unstack_vec(XX,uu)
+    X=reshape(XX,[4,length(XX)/4]);
+    u=reshape(uu,[2,length(uu)/2]);
 end
