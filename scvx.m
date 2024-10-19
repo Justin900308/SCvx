16f680b9b5c59d166863eaeddd840eb3ba5a5aca
Justin900308 <wow3987@gmail.com>
2024-10-18 22:19:34 -0700

    Add files via upload

--- scvx.m
+++ scvx.m
@@ -19,9 +19,9 @@ count=1;
 X(:,1)=[0;0;0;0];
 for t=0:Ts:5
     if t<2
-        u(:,count)=[0.1;0.5];
+        u(:,count)=1*[1;01];
     else
-        u(:,count)=[1.5;0.1];
+        u(:,count)=1*[1;1];
     end
     X(:,count+1)=Ad*X(:,count)+Bd*u(:,count);
     count=count+1;   
@@ -30,14 +30,16 @@ end
 XX=reshape(X(:,1:end-1),[length((X(:,1)))*length(X(1,:))-4,1]);
 uu=reshape(u,[length((u(:,1)))*length(u(1,:)),1]);
 z=stack_vec(X,u);
-% plot(X(1,:),X(2,:),'.')
-% hold on
+plot(X(1,:),X(2,:),'.')
+hold on
 
 %%
 N=length(X(1,:));
-obs_center=[5;2];
-R=5;
-alpha=3;
+obs_center=[5;5];
+R=3;
+alpha=1;
+
+r_default=0.8;
 
 lambda=10000;
 rho0=0.01;
@@ -45,32 +47,61 @@ rho1=0.2;
 rho2=0.9;
 i=1;
 figure(1)
-for k=1:15
-    cvx_begin
+tol=0.001;
+
+
+
+
+
+theta=linspace(0,2*pi,201);
+x_theta=R*cos(theta);
+y_theta=R*sin(theta);
+plot(obs_center(1)+x_theta,obs_center(2)+y_theta)
+for k=1:60
+    
+
+
+
+    cvx_begin quiet
         
         variable w(2,N-1)
+
         variable v(4,N-1)
         variable d(4,N)
         variable U(1,N-1)
-        variable s(N)
+        %variable s(N)
         %variable s_pos(N)
-        minimize (  0.5*sum(U) + lambda*sum(sum(abs(v)))   )
+        minimize (  500*sum(U*Ts) + lambda*sum(sum(abs(v)))   )
         subject to
         E=eye(4);
         d(:,1)==[0;0;0;0];
-        lim=2;
+        
         for i=1:N-1
             X(:,i+1)+d(:,i+1)==(Ad*X(:,i)+Ad*d(:,i))+(Bd*u(:,i)+Bd*w(:,i))+E*v(:,i);
             U(i)==norm(u(:,i),2);
-            -lim<=w(1,i)<=lim;
-            -lim<=w(2,i)<=lim;
-            R-norm(X(1:2,i)-obs_center,2)-(X(1:2,i)-obs_center)'*(X(1:2,i)+d(1:2,i)-obs_center)/norm(X(1:2,i)-obs_center,2)<=-3;
-            R-norm(X(1:2,i)-obs_center,2)-(X(1:2,i)-obs_center)'*(X(1:2,i)-obs_center)/norm(X(1:2,i)-obs_center,2)==s(i);
+            
+            -r_default<=w(1,i)<=r_default;
+            -r_default<=w(2,i)<=r_default;
+            dh=2*(X(1:2,i)-obs_center);
+            h=norm(X(1:2,i)-obs_center,2);
+            %-0.1<=d(3:4,i)<=0.1;
+
+
+            R-norm(X(1:2,i)-obs_center,2)-(X(1:2,i)-obs_center)'*(X(1:2,i)+d(1:2,i)-obs_center)/norm(X(1:2,i)-obs_center,2)<=0;
+            %R-norm(X(1:2,i)-obs_center,2)-(X(1:2,i)-obs_center)'*(X(1:2,i)-obs_center)/norm(X(1:2,i)-obs_center,2)==s(i);
         end
         
         X(:,N)+d(:,N)==[10;10;0;0];
         
     cvx_end
+
+    % 
+    for i=1:N-1
+
+        s(i)=R-norm(X(1:2,i)-obs_center,2);
+
+    end
+
     w=full(w);
     v=full(v);
     d=full(d);
@@ -78,12 +109,13 @@ for k=1:15
     u=u+w;
     hold on
     plot(X(1,:),X(2,:),'.')
+    pause(0.01)
+    if max(s)<0 && k>4
+        break;
+    end
 end
 
-theta=linspace(0,2*pi,201);
-x_theta=R*cos(theta);
-y_theta=R*sin(theta);
-plot(obs_center(1)+x_theta,obs_center(2)+y_theta)
+
 
 figure(2)
 hold on
@@ -92,6 +124,16 @@ theta=linspace(0,2*pi,201);
 x_theta=R*cos(theta);
 y_theta=R*sin(theta);
 plot(obs_center(1)+x_theta,obs_center(2)+y_theta)
+
+
+%%
+
+        for i=1:N-1
+
+            R-norm(X(1:2,i)-obs_center,2)-(X(1:2,i)-obs_center)'*(X(1:2,i)+d(1:2,i)-obs_center)/norm(X(1:2,i)-obs_center,2)
+
+        end
+        i=29
 %%
 
 %X=[5.1;5;0;0]
