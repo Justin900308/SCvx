af1d92107e594052b43c5017ec10e69141b2a18e
Justin900308 <wow3987@gmail.com>
2024-11-01 14:49:06 -0700

    Add files via upload

--- scvx_single_inte.m
+++ scvx_single_inte.m
@@ -33,12 +33,17 @@ plot(X(1,:),X(2,:),'.')
 hold on
 
 %%
+
+
+P_des=[10;10];
+
+
 N=length(X(1,:));
-obs_center=[5;3];
-R=2;
+obs_center=[5;4];
+R=3.5;
 alpha=1;
 
-r_default=5.8;
+r_default=0.3;
 
 lambda=10000;
 rho0=0.01;
@@ -56,10 +61,11 @@ theta=linspace(0,2*pi,201);
 x_theta=R*cos(theta);
 y_theta=R*sin(theta);
 plot(obs_center(1)+x_theta,obs_center(2)+y_theta)
-for k=1:10
+for k=1:100
     
 
     cvx_solver SDPT3
+    cvx_precision best
     %cvx_solver sedumi
     cvx_begin 
         
@@ -67,22 +73,26 @@ for k=1:10
 
         variable v(2,N-1)
         variable d(2,N)
-        variable U(1,N-1)
+        %variable U(2,N-1)
         variable s(N-1)
         %s=zeros(N,1);
         %variable s_pos(N)
-        minimize (  1*sum(sum(abs((u+w)*Ts))) + lambda*sum(sum(abs(v)))  + 100*lambda*sum(max(s,0)) )
-        %minimize (  500*sum(U*Ts) + lambda*sum(sum(abs(v)))  + (10*lambda-1*lambda*sum(s)) )
+        minimize (  0.1*sum(sum(abs((u+w)*Ts))) + lambda*sum(sum(abs(v)))  + 1*lambda*sum(max(s,0)) )
+        %minimize (  01*sum(sum(abs((U)*Ts))) + lambda*sum(sum(abs(v)))  + 1*lambda*sum(max(s)) )
+  
         subject to
         E=eye(2);
         
         X(:,1)+d(:,1)==[0;0];
         for i=1:N-1
-            %s(i)<=0;
+            %s(i)<=-R*0.5*0;
+            
             
             
             X(:,i+1)+d(:,i+1)==(Ad*X(:,i)+Ad*d(:,i))+(Bd*u(:,i)+Bd*w(:,i))+E*v(:,i);
-            %U(i)==u(:,i)+w(:,i);
+            %U(:,i)  ==     0.5*(P_des-X(:,i))     -    (u(:,i)+w(:,i));  % desired velocity - actual velocity
+
+
             %v_des=([10;10]-(X(1:2,i)+d(:,i)));
             %U(i)==norm([00;0]-(X(1:2,i)))  -  (u(:,i)+w(:,i));
 
@@ -101,7 +111,7 @@ for k=1:10
             %R-norm(X(1:2,i)-obs_center,2)-(X(1:2,i)-obs_center)'*(X(1:2,i)-obs_center)/norm(X(1:2,i)-obs_center,2)
         end
         
-        X(:,N)+d(:,N)==[10;10];
+        X(:,N)+d(:,N)==P_des;
         
     cvx_end
 
@@ -121,7 +131,7 @@ for k=1:10
 
     end
     pause(0.01)
-    if max(ss)<0 && k>40
+    if max(ss)<0 && k>10
         break;
     end
 end
@@ -139,77 +149,7 @@ plot(obs_center(1)+x_theta,obs_center(2)+y_theta)
 
 %%
 
-for i=1:N-1
-    AAA(i)=R-norm(X(1:2,i)-obs_center,2)-(X(1:2,i)-obs_center)'*(X(1:2,i)-obs_center)/norm(X(1:2,i)-obs_center,2);
-end
-%%
-
-%X=[5.1;5;0;0]
-% R-norm(X(1:2)-obs_center,2)-(X(1:2)-obs_center)'*(X(1:2)-obs_center)/norm(X(1:2)-obs_center,2)
-% 
-% lambda*sum(s(s>=0))
-%(X(1:2,i)-obs_center)'*(X(1:2,i)-obs_center)*d(:,i)
-
-% %%
-% obs_center=[5;5];
-% R=1;
-% alpha=1;
-% 
-% 
-% 
-% cvx_begin
-% 
-%     variable uu(102,1)
-%     variable XX(204,1)
-% 
-% 
-%     minimize(norm(uu,2))
-% 
-%     XX(1:4)==[0;0;0;0];
-%     uu(1:2)==[0;0];
-%     for i=2:51
-%         XX((i-1)*4+1:(i-1)*4+4)==Ad*XX((i-1-1)*4+1:(i-1-1)*4+4)+Bd*uu((i-1-1)*2+1:(i-1-1)*2+2);
-%         h=norm(XX((i-1-1)*4+1:(i-1-1)*4+2)-obs_center,2)-R;
-%         dh=2*(XX((i-1-1)*4+1:(i-1-1)*4+2)-obs_center)';
-% 
-%     end
-% 
-% 
-%     XX(201:204)==[10;10;0;0];
-% 
-% cvx_end
-% 
-% [X,u]=unstack_vec(XX,uu);
-% 
-
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% %% 
-% syms x y xobs yobs
-% P=[x;y]
-% P_obs=[xobs;yobs]
-% D=norm(P-P_obs)
-% pretty(diff(D,x))
-% pretty(diff(D,y))
 
 
 
-%% utility funcitons
 
-
-function XX=stack_vec(X,u)
-    XX=reshape(X(:,1:end-1),[length((X(:,1)))*length(X(1,:))-4,1]);
-    uu=reshape(u,[length((u(:,1)))*length(u(1,:)),1]);
-    XX=[XX;uu];
-end
-
-
-function [X,u]=unstack_vec(XX,uu)
-    X=reshape(XX,[4,length(XX)/4]);
-    u=reshape(uu,[2,length(uu)/2]);
-end
